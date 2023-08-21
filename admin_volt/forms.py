fceaff2af9c1a9cfd137a4c33d9055383a21d403
jerome <jerome910810@gmail.com>
2023-08-21 13:44:59 +0800

    tidy

--- admin_volt/forms.py
+++ admin_volt/forms.py
@@ -5,38 +5,38 @@ from django.utils.translation import gettext_lazy as _
 
 
 class RegistrationForm(UserCreationForm):
-  password1 = forms.CharField(
-      label=_("Password"),
-      widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}),
-  )
-  password2 = forms.CharField(
-      label=_("Confirm Password"),
-      widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm Password'}),
-  )
+    password1 = forms.CharField(
+        label=_("Password"),
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}),
+    )
+    password2 = forms.CharField(
+        label=_("Confirm Password"),
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm Password'}),
+    )
 
-  class Meta:
+class Meta:
     model = User
     fields = ('username', 'email', )
 
     widgets = {
-      'username': forms.TextInput(attrs={
-          'class': 'form-control',
-          'placeholder': 'Username'
-      }),
-      'email': forms.EmailInput(attrs={
-          'class': 'form-control',
-          'placeholder': 'example@company.com'
-      })
+        'username': forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Username'
+        }),
+        'email': forms.EmailInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'example@company.com'
+        })
     }
 
 
 class LoginForm(AuthenticationForm):
-  username = UsernameField(label=_("Your Username"), widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Username"}))
-  password = forms.CharField(
-      label=_("Your Password"),
-      strip=False,
-      widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": "Password"}),
-  )
+    username = UsernameField(label=_("Your Username"), widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Username"}))
+    password = forms.CharField(
+    label=_("Your Password"),
+    strip=False,
+    widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": "Password"}),
+    )
 
 class UserPasswordResetForm(PasswordResetForm):
     email = forms.EmailField(widget=forms.EmailInput(attrs={
