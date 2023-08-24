9f2b522fb080a8ae39ba735ec66909b00a20f7d1
yoyolien <yoyomm99@gmail.com>
2023-08-24 22:33:32 +0800

    電力計算

--- home/urls.py
+++ home/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('index/', views.index, name="index"),
     path("upload/",views.upload_data_view,name="upload_data"),
     # path("requestml/",views.requestmlresult),
-    path("requestchart/",views.chart),
 #
 ]
